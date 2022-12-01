abe820805602245cfc0ac6e560b0494737ef158a
Toolback <93286025+Toolback@users.noreply.github.com>
2022-12-01 12:02:00 +0100

    fix stats.stats

--- components/app/StatsCards.tsx
+++ components/app/StatsCards.tsx
@@ -7,7 +7,7 @@ const StatsCards = (stats?: any) => {
   const [actualStats, setActualStats] = useState([null]);
 
   useEffect(() => {
-    setActualStats(stats);
+    setActualStats(stats.stats);
     renderStats()
   }, [stats]);
 
@@ -20,7 +20,7 @@ const StatsCards = (stats?: any) => {
           <>
             <dl className="grid grid-cols-1 gap-5 sm:grid-cols-4">
 
-              {actualStats?.stats?.map((item: any) => (
+              {actualStats?.map((item: any) => (
                 <div key={item.name} className="overflow-hidden rounded-lg bg-white bg-opacity-10 px-4 py-5 shadow sm:p-6">
                   <dt className="truncate text-sm font-medium text-gray-300">{item.name}</dt>
                   <dd className="mt-1 text-3xl font-semibold tracking-tight">{item.stat}</dd>
--- sections/app/Pools.jsx
+++ sections/app/Pools.jsx
@@ -8,33 +8,33 @@ const Pools = () => {
             name: "Usdt",
             infos: "Safe Strategy",
             ibTokenAddress: "0x0",
-            tvl: "100'000", 
-            image:""
+            tvl: "100'000",
+            image: ""
         },
         {
             asset: "Dai",
             ibTokenAddress: "0x0",
-            tvl: "0", 
+            tvl: "0",
         }
     ]
     const stats = [
-            { name: 'Total Deposit', stat: 'N/a' },
-            { name: 'Total Claimable', stat: 'N/a' },
-            { name: 'Total Earned', stat: 'N/a' },
-            { name: 'Total IbTokens', stat: 'N/a' }
-          ]
+        { name: 'Total Deposit', stat: 'N/a' },
+        { name: 'Total Claimable', stat: 'N/a' },
+        { name: 'Total Earned', stat: 'N/a' },
+        { name: 'Total IbTokens', stat: 'N/a' }
+    ]
     return (
         <>
-        <div className="hidden sm:block">
+            <div className="hidden sm:block">
                 <h3 className="pb-2 text-xl font-semibold text-white">My Summary</h3>
-            <StatsCards  stats={stats}/>
-        </div>
-        <PoolsNews />
-        <PoolsTable availablePools={availablePools}/>
-        <div className="sm:hidden">
+                <StatsCards stats={stats} />
+            </div>
+            <PoolsNews />
+            <PoolsTable availablePools={availablePools} />
+            <div className="sm:hidden">
                 <h3 className="pb-2 text-xl font-semibold text-white">My Summary</h3>
-            <StatsCards  stats={stats}/>
-        </div>
+                <StatsCards stats={stats} />
+            </div>
         </>
     )
 }
