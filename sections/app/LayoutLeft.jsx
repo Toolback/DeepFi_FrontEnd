84eab4feeab8fcc9e208862765161ec859e6cd3d
toolback <julien.rnlt@gmail.com>
2023-03-01 13:00:02 +0100

    update

--- sections/app/LayoutLeft.jsx
+++ sections/app/LayoutLeft.jsx
@@ -16,27 +16,50 @@ const LayoutLeft = () => {
             <div className='fixed h-screen pl-6'>
                 <div className='flex flex-col h-screen justify-between '>
 
-                      <Link href="/">
-            <button type="button">
-                <h2 className="pt-2 font-extrabold text-[24px] leading-[30.24px] ">
-                    Deposit.Finance
-                </h2>
-            </button>
-          </Link>
-            <div className='flex flex-col gap-2 font-normal sm:text-[23px] text-[20px] '>
-                {/* <button className="hover:font-extrabold" onClick={() => handleNavClick('news')}>News</button> */}
-                <button className="hover:font-extrabold" onClick={() => handleNavClick('dashboard')}>Dashboard</button>
-                <button className="hover:font-extrabold" onClick={() => handleNavClick('stake')}>Stake</button>
-                <button className="hover:font-extrabold" onClick={() => handleNavClick('bonds')}>Bonds</button>
+                    <Link href="/">
+                        <button type="button">
+                            <h2 className="pt-2 font-extrabold text-[24px] leading-[30.24px] ">
+                                Deposit.Finance
+                            </h2>
+                        </button>
+                    </Link>
+                    <div className='flex flex-col gap-2 font-normal sm:text-[23px] text-[20px] '>
+                        {/* <button className="hover:font-extrabold" onClick={() => handleNavClick('news')}>News</button> */}
+                        <button className="hover:font-extrabold" onClick={() => handleNavClick('dashboard')}>Dashboard</button>
+                        <button className="hover:font-extrabold" onClick={() => handleNavClick('stake')}>Stake</button>
+                        <button className="hover:font-extrabold" onClick={() => handleNavClick('bonds')}>Bonds</button>
 
-            </div>
-            <div className='flex flex-col'>
-                <button className="hover:font-extrabold" onClick={() => handleNavClick('stake')}>Docs</button>
-                <button className="hover:font-extrabold" onClick={() => handleNavClick('bonds')}>Help</button>
-                <Link href="/"><button>Home</button></Link>
-            </div>
+                    </div>
+                    <div className='flex pb-6 gap-4 justify-center'>
+                        <button className="hover:font-extrabold" onClick={() => handleNavClick('stake')}>
+                            <img
+                                // key={social.name}
+                                src="iconDocs.png"
+                                // alt={social.name}
+                                className="w-[24px] h-[24px] object-contain cursor-pointer"
+                            />
+                        </button>
+                        <button className="hover:font-extrabold" onClick={() => handleNavClick('bonds')}>
+                            <img
+                                // key={social.name}
+                                src="iconQuestions.png"
+                                // alt={social.name}
+                                className="w-[24px] h-[24px] object-contain cursor-pointer"
+                            />
+                        </button>
+                        <Link href="/">
+                            <button>
+                                <img
+                                    // key={social.name}
+                                    src="iconHome.png"
+                                    // alt={social.name}
+                                    className="w-[24px] h-[24px] object-contain cursor-pointer"
+                                />
+                            </button>
+                        </Link>
+                    </div>
                 </div>
-        </div>
+            </div>
         </div>
     )
 }
