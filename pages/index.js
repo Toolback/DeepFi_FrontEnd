9644674e3aa6d79e95c952582171d88ba10fca36
toolback <jurenaul@student.42lausanne.ch>
2023-05-09 18:48:16 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import { Footer, Navbar } from '../components/home';
 import { About, Explore, RoadMap, GetStarted, Hero, Insights, WhatsNew, World } from '../sections/home';
+import HomePage from '../sections/home/HomePage';
 
 const Home = () => (
-  <div className="bg-primary-black overflow-hidden">
+  <section className="w-full h-screen bg-primary-black ">
+  {/* // <div className="bg-cover bg-center" style={{backgroundImage: `url(/HomeDeposit.png)`}}> */}
+
+  <img
+      src="/HomeDeposit.png"
+      alt="hero_cover"
+      // className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
+      className="absolute object-cover h-full w-full opacity-50"
+
+    />
     <Navbar />
-    <Hero />
+    {/* <Hero />
     <div className="relative">
       <About />
       <div className="gradient-03 z-0" />
@@ -14,15 +24,15 @@ const Home = () => (
       <div className="gradient-04 z-0" />
       <GetStarted />
     </div>
-    {/* <World /> */}
     <div className="relative">
-      {/* <Explore /> */}
-      {/* <Insights /> */}
+      //<Explore />
+      //<Insights />
       <div className="gradient-04 z-0" />
       <RoadMap />
-    </div>
-    <Footer />
-  </div>
+    </div> */}
+    <HomePage />
+    {/* <Footer /> */}
+  </section>
 );
 
 export default Home;
